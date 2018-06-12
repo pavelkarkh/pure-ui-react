f7025536541c877609d69b0a046bf61eec6760dc
Pavel Karkh <pavelkarkh@gmail.com>
2018-06-12 10:29:35 +0500

    add Button

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,7 +1,100 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import PropTypes from 'prop-types'
+import cn from 'classnames'
 
-export default function Button() {
-  return (
-    <button className="pure-button">A Pure Button</button>
-  )
+import '../../node_modules/purecss/build/buttons-core-min.css'
+import '../../node_modules/purecss/build/buttons-min.css'
+import './button.css'
+
+class Button extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    const {
+      disabled,
+      href,
+      children,
+      active,
+      primary,
+      size,
+      success,
+      error,
+      warning,
+      secondary,
+      custom,
+      icon,
+      ...props
+    } = this.props
+    
+    let sizeClassName
+    if (size) {  
+      switch(size) {
+      case 'xsmall':
+        sizeClassName = 'button-xsmall'
+        break
+      case 'small':
+        sizeClassName = 'button-small'
+        break
+      case 'large':
+        sizeClassName = 'button-large'
+        break
+      case 'xlarge':
+        sizeClassName = 'button-xlarge'
+        break
+      }
+    }
+
+    const style = cn(
+      sizeClassName,
+      {
+        'button-success': success,
+        'button-error': error,
+        'button-warning': warning,
+        'button-secondary': secondary
+      },
+      'pure-button',
+      {
+        'pure-button-disabled': disabled,
+        'pure-button-active': active,
+        'pure-button-primary': primary
+      },
+      custom
+    )
+
+    let iconElement
+    if (icon) {
+      const iconStyle = cn('icon', icon)
+      iconElement = React.createElement('i', { className: iconStyle }, null)
+    }
+
+    return React.createElement(
+      href ? 'a' : 'button',
+      {
+        className: style,
+        ...props
+      },
+      iconElement,
+      children
+    )
+  }
 }
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  href: PropTypes.string,
+  children: PropTypes.node,
+  active: PropTypes.bool,
+  primary: PropTypes.bool,
+  size: PropTypes.oneOf(['xsmall', 'small', 'large', 'xlarge']),
+  success: PropTypes.bool,
+  error: PropTypes.bool,
+  warning: PropTypes.bool,
+  secondary: PropTypes.bool,
+  custom: PropTypes.string,
+  icon: PropTypes.string,
+}
+
+export default Button
--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,26 @@ const loaders = {
         presets: ['babel-preset-env', 'babel-preset-stage-2', 'react']
       }
     }
+  },
+  style: {
+    test: /\.css?$/,
+    use: ['style-loader', 'css-loader']
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 module.exports = env => {
   return {
     entry: './src/index.js',
     output: {
+      // eslint-disable-next-line no-undef
       path: path.resolve(__dirname),
       filename: 'index.js',
       library: 'pure-ui-react',
       libraryTarget: 'umd',
     },
     module: {
-      rules: [loaders.babel],
+      rules: [loaders.babel, loaders.style],
     },
     resolve: {
       extensions: ['.js', '.jsx', '.json', '*'],
